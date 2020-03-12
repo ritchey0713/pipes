c2e01c0f3e05d5155de34b4ec278effcd15866f3
Dj <ritchey0713@gmail.com>
2020-03-12 19:04:35 -0400

    add pipe logic

--- src/app/miles-convert.pipe.ts
+++ src/app/miles-convert.pipe.ts
@@ -5,8 +5,11 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class MilesConvertPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: any, ...args: unknown[]): unknown {
+    if(!value){
+      return
+    }
+    return value * 1.60934;
   }
 
 }
