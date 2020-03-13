f5df9f91aa6a8c97dd563d8d1f49ef8787c9fd98
Dj <ritchey0713@gmail.com>
2020-03-13 10:55:53 -0400

    add custom args to pipe

--- src/app/miles-convert.pipe.ts
+++ src/app/miles-convert.pipe.ts
@@ -5,11 +5,21 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class MilesConvertPipe implements PipeTransform {
 
-  transform(value: any, ...args: unknown[]): unknown {
+  transform(value: any, targetUnits: string): any {
     if(!value){
       return
     }
-    return value * 1.60934;
+
+    switch(targetUnits){
+      case 'km':
+      return value * 1.60934;
+      case "m":
+        return value * 1.60934 * 1000;
+      case "cm":
+      return value * 1.60934 * 1000 * 1000;
+      default:
+        throw new Error("target unit not found!")
+    }
   }
 
 }
